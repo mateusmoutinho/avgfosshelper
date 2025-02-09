03c246f67b3e96f09b22e835757726b43c78f656
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2025-02-09 02:26:25 -0300

    ignore build/*

--- build/CMakeFiles/dtw_object.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dtw_object.dir/dtw.c.o"
-  "CMakeFiles/dtw_object.dir/dtw.c.o.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/dtw_object.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/sha256_object.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sha256_object.dir/sha-256.c.o"
-  "CMakeFiles/sha256_object.dir/sha-256.c.o.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/sha256_object.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
