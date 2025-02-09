b36b3be871bd1aae8d07ec2b66feccec1748ddcb
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-02-09 04:08:52 -0300

    att

--- teste.c
+++
@@ -1,6 +0,0 @@
-#include "DoTheWorld/src/one.c"
-
-int main(){
-
-   dtw_get_entity_last_motification_in_unix
-}
--- teste.py
+++
@@ -1,6 +0,0 @@
-
-r = ["main.c", "main.h", "funcoes.c", "funcoes.h", "teste.c", "teste.h"]
-espaco  = {
-    ["main.c",1000],
-    ["main.h",1500],
-}
