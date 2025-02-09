12dc8afbbc895805f8c9ebb6f24c3aa1f3ecd5f3
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-02-09 01:36:51 -0300

    att

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 
 gcc -c dtw.c -o dtw.o
 gcc -c sha-256.c -o sha256.o
-gcc -c funcs.c -o funcs.o
-gcc  main.c dtw.o sha256.o funcs.o -o main.out
+gcc  main.c dtw.o sha256.o  -o main.out
--- main.c
+++ main.c
@@ -8,7 +8,6 @@
 #include <sys/stat.h>
 #include <stdio.h>
 #include "dtw.h"
-#include "funcs.h"
 #include "sha-256.h"
 //cache folder
 #define  CF ".cache"
@@ -26,13 +25,13 @@ int main(int argc, char *argv[]){
     //repo to clone
     char *rpc = argv[1];
     //check if the repo name is too long
-    if(gsl(rpc) > 900){
+    if(strlen(rpc) > 900){
         printf("The repo name is too long\n");
         return 1;
     }
     //repo sha
     uint8_t rsv[32] = {0};
-    calc_sha_256(rsv,rpc,gsl(rpc));
+    calc_sha_256(rsv,rpc,strlen(rpc));
     ///hash of the repo url
     char hs[60] ={0}; 
     for (unsigned int i = 0; i < SIZE_OF_SHA_256_HASH; i++) {
@@ -184,7 +183,7 @@ int main(int argc, char *argv[]){
     }
     
     //===============================spull check========================================
-    
+
     return 0;
     //comand 
     
